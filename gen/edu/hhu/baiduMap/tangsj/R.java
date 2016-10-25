854c803ec94122663d55395541b97529b7cabf87
Tangworld <948138224@qq.com>
2016-10-25 20:00:09 +0800

    完成了登录页面

--- gen/edu/hhu/baiduMap/tangsj/R.java
+++ gen/edu/hhu/baiduMap/tangsj/R.java
@@ -10,19 +10,55 @@ package edu.hhu.baiduMap.tangsj;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splash=0x7f020001;
-        public static final int splash2=0x7f020002;
-        public static final int splash3=0x7f020003;
-        public static final int splash4=0x7f020004;
+        public static final int background_button_div=0x7f020000;
+        public static final int background_edit=0x7f020001;
+        public static final int background_editbutton=0x7f020002;
+        public static final int background_login=0x7f020003;
+        public static final int background_login_div=0x7f020004;
+        public static final int bkg=0x7f020005;
+        public static final int bkg2=0x7f020006;
+        public static final int bkg3=0x7f020007;
+        public static final int bkg4=0x7f020008;
+        public static final int bkg5=0x7f020009;
+        public static final int bkg6=0x7f02000a;
+        public static final int bkg7=0x7f02000b;
+        public static final int button1=0x7f02000c;
+        public static final int button2=0x7f02000d;
+        public static final int button3=0x7f02000e;
+        public static final int button4=0x7f02000f;
+        public static final int ic_launcher=0x7f020010;
+        public static final int icon_marka=0x7f020011;
+        public static final int input_over=0x7f020012;
+        public static final int login_input=0x7f020013;
+        public static final int logo=0x7f020014;
+        public static final int qq_edit_login=0x7f020015;
+        public static final int splash=0x7f020016;
+        public static final int splash2=0x7f020017;
+        public static final int splash3=0x7f020018;
+        public static final int splash4=0x7f020019;
     }
     public static final class id {
-        public static final int editpassword=0x7f060003;
-        public static final int editusername=0x7f060001;
-        public static final int enter=0x7f060004;
-        public static final int password=0x7f060002;
-        public static final int username=0x7f060000;
+        public static final int btnLogin=0x7f07000b;
+        public static final int btnReg=0x7f07000c;
+        public static final int buttonPanel=0x7f07000a;
+        public static final int check1=0x7f070008;
+        public static final int check2=0x7f070009;
+        public static final int checkPanel=0x7f070007;
+        public static final int enter=0x7f07000d;
+        public static final int loginPanel=0x7f070001;
+        public static final int myappname=0x7f070002;
+        public static final int pass=0x7f070006;
+        public static final int passwordPanel=0x7f070005;
+        public static final int screen=0x7f070000;
+        public static final int user=0x7f070004;
+        public static final int usernamePanel=0x7f070003;
     }
     public static final class layout {
         public static final int login=0x7f030000;
@@ -32,9 +68,20 @@ public final class R {
         public static final int splash4=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int password=0x7f040002;
-        public static final int username=0x7f040001;
+        public static final int accout=0x7f050004;
+        public static final int action_settings=0x7f050003;
+        public static final int app_name=0x7f050000;
+        public static final int content=0x7f05000a;
+        public static final int login=0x7f050006;
+        public static final int logintheme=0x7f05000b;
+        public static final int password=0x7f050002;
+        public static final int regist=0x7f050007;
+        public static final int remempass=0x7f05000d;
+        public static final int rememuser=0x7f05000c;
+        public static final int repeatpassword=0x7f050005;
+        public static final int save=0x7f050009;
+        public static final int title=0x7f050008;
+        public static final int username=0x7f050001;
     }
     public static final class style {
         /** 
@@ -58,10 +105,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
